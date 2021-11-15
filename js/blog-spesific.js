5fd3c514156c8c4b638b0720392586974d5e6faf
Ingsy <ingeborg_mellingen89@hotmail.com>
2021-11-15 18:38:06 +0100

    test

--- js/blog-spesific.js
+++ js/blog-spesific.js
@@ -1,34 +1,68 @@
-function getPosts() {
-    const url = "http://projectexamingsy.local/wp-json/wp/v2/posts/";
-    fetch(url)
-      .then((response) => response.json())
-      .then((list) => {
-        let html = "";
-        for (let i = 0; i < list.length; i++) {
-          const title = title.rendered;
-          const text = content.rendered;
-  
-          /* const product = list[i];
-  
-          const id = product.id;
-          const title = product.name;
-          const description = product.short_description;
-          const price = product.price_html;
-          const imageUrl = product.images[0].src;
-          const color = "red"; //product.attributes.color
-          const div = `
-                <div class="jacket">
-                  <a href="jacket-spesific.html?id=${id}"><img src="${imageUrl}" /></a>
-                  <button onclick="addToChart("${color}")">Add to chart</button>
-                  <span>${title}</span> </br>
-                  <span>${price}</span>
-                </div>
-              `;
-          html = html + div;
-        }
-        document.querySelector(".container").innerHTML += html;
-      })
-      .catch((error) => {});
-  }
-  
-  getProducts();
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+const url = `http://projectexamingsy.local/wp-json/wp/v2/posts/${id}`;
+fetch(url)
+  .then((resp2) => resp2.json())
+  .then((product) => {
+    document.querySelector("#jacket-name").innerHTML = product.name;
+    document.querySelector(".jacket-area").innerHTML = `
+        <div></div>
+            <div class="big grey-light-bg cc-column">
+              <img
+                src="./Img/jacket-spesific/windbreaker-jacket-big.png"
+                alt="windbreaker jacket"
+              />
+            </div>
+            <div class="selection">
+              <div class="cc-column">
+                <img
+                  src="./Img/jacket-spesific/windbreaker-jacket-down.png"
+                  alt="windbreaker jacket"
+                />
+              </div>
+              <div class="cc-column">
+                <img
+                  src="./Img/jacket-spesific/windbreaker-jacket-small.png"
+                  alt="windbreaker jacket"
+                />
+              </div>
+              <div class="cc-column">
+                <img
+                  src="./Img/jacket-spesific/windbreaker-jacket-right.png"
+                  alt="windbreaker jacket"
+                />
+              </div>
+              <div class="cc-column">
+                <img
+                  src="./Img/jacket-spesific/windbreaker-jacket-left.png"
+                  alt="windbreaker jacket"
+                />
+              </div>
+            </div>
+        `;
+    document.querySelector(".colors-area").innerHTML = `
+        <div class="colors-area-text cc-column">
+              <div>${product.name}</div>
+              <div>${product.price_html}</div>
+            </div>
+            <div class="color-area-selection">
+              <div class="yellow-bg box"></div>
+              <div class="green-bg box"></div>
+              <div class="black-bg box"></div>
+              <div class="grey-bg box"></div>
+            </div>
+            <div class="color-area-footer cc-column">
+              <div>S M L</div>
+              <button class="button-color" onclick="addToChart('yellow')">
+                Add to chart
+              </button>
+
+              <div class="description cc-column">
+                <div>Description</div>
+                <div class="desk">Category</div>
+                <div class="desk">Details</div>
+              </div>
+            </div>
+        `;
+  });
--- js/script.js
+++ js/script.js
@@ -6,3 +6,33 @@ carouselposts.forEach(function (post) {
     clickParent.classList.add("selected-post");
   };
 });
+
+function getPosts() {
+  const url = "http://projectexamingsy.local/wp-json/wp/v2/posts/";
+  fetch(url)
+    .then((response) => response.json())
+    .then((list) => {
+      let html = "";
+      for (let i = 0; i < list.length; i++) {
+        const title = title.rendered;
+        const text = content.rendered;
+        const logo = guide.rendered;
+        const alt = caption.alt_text;
+        const web = caption.rendered;
+        const div = ` <figure class="selected-post latest-post cc-column">
+<h3>${title}</h3>
+<img
+  src="${logo}"
+  alt="Ikea-logo"
+/>
+<h3>read more</h3>
+</figure> 
+`;
+        html = html + div;
+      }
+      document.querySelector(".container").innerHTML += html;
+    })
+    .catch((error) => {});
+}
+
+getProducts();
