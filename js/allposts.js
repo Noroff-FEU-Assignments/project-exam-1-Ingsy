0e548ba36bb6ef1fe9930cc93ee3d098f50084e9
Ingsy <ingeborg_mellingen89@hotmail.com>
2021-11-18 08:31:03 +0100

    js

--- js/allposts.js
+++ js/allposts.js
@@ -5,65 +5,64 @@ async function getPosts() {
   fetch(url)
     .then(async (response) => response.json())
     .then(async (list) => {
-      let html = "";
       for (let i = 0; i < list.length; i++) {
-        const title = title.rendered;
-        const text = content.rendered;
+        const post = list[i];
+        const description = "Bla";
+        const title = "Title";
         const imageRequest = await fetch(
           "http://projectexamingsy.local/wp-json/wp/v2/media/" +
             post.featured_media
         );
+        const imageData = await imageRequest.json();
 
-        const div =
-          (document.querySelector(".spesific-container").innerHTML = html;
-          document.querySelector(".container-one").innerHTML =
-            `
-      <div class="blog-spesific">
-      <h1>${post.title.rendered}</h1>
-      <p class="lorem">
-      ${post.content.rendered}
-      </p>
-    </div>
-          `);
-        document.querySelector(".blog-spesific").innerHTML = `
-      <div><img ${post.featured_media} class="img-logo" /></div>
-      <section class="blog-spesific-two">
-        <div class="bold">
-          <div class="item">go to site:</div>
-          <div class="item">discount:</div>
-          <div class="item">student discount:</div>
-          <div class="item">available on app:</div>
+        const div = `
+        div class="container-one">
+        <div class="blog-spesific">
+          <h1>${title}</h1>
+          <p class="lorem">
+           ${description}
+          </p>
         </div>
-        <div class="margin">
-          <div class="item">
-            <a href="https://www.zalando.no">www.zalando.no</a>
-          </div>
-          <div class="item">10% when signup newsletter</div>
-          <div class="item">no student discount</div>
-          <div>Available</div>
+        <div class="blog-spesific">
+          <div><img src="${imageData.guid.rendered}" class="img-logo" /></div>
+          <section class="blog-spesific-two">
+            <div class="bold">
+              <div class="item">go to site:</div>
+              <div class="item">discount:</div>
+              <div class="item">student discount:</div>
+              <div class="item">available on app:</div>
+            </div>
+            <div class="margin">
+              <div class="item">
+                <a href="https://www.zalando.no">www.zalando.no</a>
+              </div>
+              <div class="item">10% when signup newsletter</div>
+              <div class="item">no student discount</div>
+              <div>Available</div>
+            </div>
+          </section>
+          <h2>Shipping & Shopping details</h2>
+          <section class="blog-spesific-two">
+            <div class="bold">
+              <div class="item">Shipping cost:</div>
+              <div class="item">Return & refunds:</div>
+              <div class="item2">Payment options:</div>
+              <div class="item">Shipping-to-location:</div>
+              <div>Track order:</div>
+            </div>
+            <div class="margin">
+              <div class="item">kr 29,-</div>
+              <div>30 days/full refund</div>
+              <div><img src="images/payment.jpg" class="img-size" /></div>
+              <div class="item">Europa</div>
+              <div>Available</div>
+            </div>
+          </section>
         </div>
-      </section>
-      <h2>Shipping & Shopping details</h2>
-      <section class="blog-spesific-two">
-        <div class="bold">
-          <div class="item">Shipping cost:</div>
-          <div class="item">Return & refunds:</div>
-          <div class="item2">Payment options:</div>
-          <div class="item">Shipping-to-location:</div>
-          <div>Track order:</div>
-        </div>
-        <div class="margin">
-          <div class="item">kr 29,-</div>
-          <div>30 days/full refund</div>
-          <div><img src="images/payment.jpg" class="img-size" /></div>
-          <div class="item">Europa</div>
-          <div>Available</div>
-        </div>
-      </section>
+      </div>
           `;
-        html = html + div;
+        document.querySelector(".blog-posts").innerHTML += div;
       }
-      document.querySelector(".blog-spesific").innerHTML += html;
     })
     .catch((error) => {});
 }
---
+++ js/posts.js
@@ -0,0 +1,80 @@
+async function getAllPosts() {
+  fetch(`http://projectexamingsy.local/wp-json/wp/v2/posts`)
+    .then(async (resp) => resp.json())
+    .then(async (data) => {
+      for (let i = 0; i < data.length; i++) {
+        const post = data[i];
+        document.querySelector(".spesific-container").innerHTML +=
+          await renderPost(post, true);
+      }
+    });
+}
+
+async function getSinglePost(id) {
+  fetch(`http://projectexamingsy.local/wp-json/wp/v2/posts/${id}`)
+    .then(async (resp) => resp.json())
+    .then(async (data) => {
+      document.querySelector(".spesific-container").innerHTML +=
+        await renderPost(data, false);
+    });
+}
+
+async function renderPost(post, includeUrl) {
+  const description = "Bla";
+  const title = "Title";
+  const imageRequest = await fetch(
+    "http://projectexamingsy.local/wp-json/wp/v2/media/" + post.featured_media
+  );
+  const imageData = await imageRequest.json();
+  return `
+  <div class="container-one">
+  <div class="blog-spesific">
+    <h1>${title}</h1>
+    <p class="lorem">
+     ${description}
+    </p>
+    ${
+      includeUrl
+        ? `<a href="/blog-spesific.html?id=${post.id}">les mer</a>`
+        : ""
+    }
+  </div>
+  <div class="blog-spesific">
+    <div><img src="${imageData.guid.rendered}" class="img-logo" /></div>
+    <section class="blog-spesific-two">
+      <div class="bold">
+        <div class="item">go to site:</div>
+        <div class="item">discount:</div>
+        <div class="item">student discount:</div>
+        <div class="item">available on app:</div>
+      </div>
+      <div class="margin">
+        <div class="item">
+          <a href="https://www.zalando.no">www.zalando.no</a>
+        </div>
+        <div class="item">10% when signup newsletter</div>
+        <div class="item">no student discount</div>
+        <div>Available</div>
+      </div>
+    </section>
+    <h2>Shipping & Shopping details</h2>
+    <section class="blog-spesific-two">
+      <div class="bold">
+        <div class="item">Shipping cost:</div>
+        <div class="item">Return & refunds:</div>
+        <div class="item2">Payment options:</div>
+        <div class="item">Shipping-to-location:</div>
+        <div>Track order:</div>
+      </div>
+      <div class="margin">
+        <div class="item">kr 29,-</div>
+        <div>30 days/full refund</div>
+        <div><img src="images/payment.jpg" class="img-size" /></div>
+        <div class="item">Europa</div>
+        <div>Available</div>
+      </div>
+    </section>
+  </div>
+</div>
+    `;
+}
